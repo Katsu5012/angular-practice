e08f061d37c9b03a9abf5ca199ac94c8b2c898b3
Katsu5012 <laugh_katsu_love89@yahoo.co.jp>
2023-03-22 00:46:31 +0900

    feat: httpを用いたshipping priceの取得の実装

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,8 +10,9 @@ import { TopBarComponent } from './top-bar/top-bar.component';
 import { ProductAlertsComponent } from './product-alerts/product-alerts.component';
 import { ProductDetailsComponent } from './product-details/product-details.component';
 import {ReactiveFormsModule} from "@angular/forms";
-import {RouterModule} from "@angular/router";
 import { CartComponent } from './cart/cart.component';
+import { HttpClientModule } from '@angular/common/http';
+import { ShippingComponent } from './shipping/shipping.component';
 
 @NgModule({
   declarations: [
@@ -21,9 +22,11 @@ import { CartComponent } from './cart/cart.component';
     ProductAlertsComponent,
     ProductDetailsComponent,
     CartComponent,
+    ShippingComponent,
   ],
   imports: [
     BrowserModule,
+    HttpClientModule,
     AppRoutingModule,
     BrowserAnimationsModule,
     MatSlideToggleModule,
